07926951cfc7ae0edfa53dae610fbcc6cea6d5cd
YashTripathi03 <80109678+YashTripathi03@users.noreply.github.com>
2021-03-08 18:05:23 +0530

    Add files via upload

---
+++ Among_us.py
@@ -0,0 +1,125 @@
+import turtle
+
+body_color = 'red'
+body_shadow = ''
+glass_color = 'skyblue'
+glass_shadow = ''
+
+s = turtle.getscreen()
+t = turtle.Turtle()
+
+
+def body():
+    t.pensize(20)
+    # t.speed(15)
+
+    t.fillcolor(body_color)
+    t.begin_fill()
+
+    # right side
+    t.right(90)
+    t.forward(50)
+    t.right(180)
+    t.circle(40, -180)
+    t.right(180)
+    t.forward(200)
+
+    # head curve
+    t.right(180)
+    t.circle(100, -180)
+
+    # left side
+    t.backward(20)
+    t.left(15)
+    t.circle(500, -20)
+    t.backward(20)
+
+    # t.backward(200)
+    t.circle(40, -180)
+
+    # t.right(90)
+    t.left(7)
+    t.backward(50)
+
+    # hip
+    t.up()
+    t.left(90)
+    t.forward(10)
+    t.right(90)
+    t.down()
+
+    # t.right(180)
+
+    #t.circle(50, -70)
+
+    t.right(240)
+    t.circle(50, -70)
+    t.end_fill()
+
+
+def glass():
+    t.up()
+    t.right(230)
+    t.forward(100)
+    t.left(90)
+    t.forward(20)
+    t.right(90)
+    t.down()
+    t.fillcolor(glass_color)
+    t.begin_fill()
+    t.right(150)
+    t.circle(90, -55)
+    t.right(180)
+    t.forward(1)
+    t.right(180)
+    t.circle(10, -65)
+    t.right(180)
+    t.forward(110)
+    t.right(180)
+
+    # t.right(180)
+    t.circle(50, -190)
+    t.right(170)
+    t.forward(80)
+    t.right(180)
+    t.circle(45, -30)
+    t.end_fill()
+
+
+def backpack():
+    t.up()
+    t.right(60)
+    t.forward(100)
+    t.right(90)
+    t.forward(75)
+    t.fillcolor(body_color)
+    t.begin_fill()
+    t.down()
+    t.forward(30)
+    t.right(255)
+    t.circle(300, -30)
+    t.right(260)
+    t.forward(30)
+    t.end_fill()
+    t.hideturtle()
+
+
+def text():
+    t.penup()
+    t.goto(0, 250)
+    t.color('black')
+    style = ('Courier', 30, 'bold')
+    t.write('Imposter', font=style, align='center')
+    t.hideturtle()
+    t.penup()
+    t.goto(0, -200)
+    t.color('black')
+    style = ('Courier', 30, 'bold')
+    t.write('We have an imposter!', font=style, align='center')
+    t.hideturtle()
+
+
+body()
+glass()
+backpack()
+text()
